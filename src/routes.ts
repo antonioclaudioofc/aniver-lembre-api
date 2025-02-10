4f5b073ac9a3ee1462c891a32e148105f61553ac
Antonio Claudio <claudioalvesdev@gmail.com>
2025-02-10 13:16:39 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,50 @@
+import z from "zod";
+import { FastifyTypedInstance } from "./types";
+import { randomUUID } from "node:crypto";
+import { db } from "./firebase";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const users = [];
+
+export async function routes(app: FastifyTypedInstance) {
+  app.get(
+    "/users",
+    {
+      schema: {
+        tags: ["users"],
+        description: "List users",
+      },
+    },
+    async () => {
+      const snapshot = await db.collection("users").get();
+      return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    }
+  );
+
+  app.post(
+    "/users",
+    {
+      schema: {
+        tags: ["users"],
+        description: "Create a new user",
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+        }),
+        response: "Create user",
+      },
+    },
+    async (request, reply) => {
+      const { name, email } = request.body;
+      const id = randomUUID();
+
+      await db.collection("users").doc(id).set({ name, email });
+      return reply.status(201).send({ id, name, email });
+    }
+  );
+}
